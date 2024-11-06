2eaf0d86301380278813fe34542c73d8159a8661
Konstantin <bkv-83@yandex.ru>
2024-11-06 19:45:09 +0300

    добавил гитигнор и список библиотек

--- prediction.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-# Загружаем обученную модель
-model = joblib.load('procurement_classification_model.pkl')
-
-# Функция для предсказания класса закупки
-def predict_class(input_data):
-    return model.predict([input_data])[0]
